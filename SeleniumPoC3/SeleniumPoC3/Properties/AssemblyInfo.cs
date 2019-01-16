a5c2a3073a1c5453fd297b4871d1b67dfaadea5e
Rathijit Sarkar <rathijit.sarkar@gmail.com>
2019-01-15 23:34:05 -0800

    initial commit

---
+++ SeleniumPoC3/SeleniumPoC3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumPoC3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumPoC3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa32d70c-f829-48be-a0d1-f6f58454f86a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumPoC3/SeleniumPoC3/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumPoC3
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\svc_surfqacoe\Documents");
+            driver.Navigate().GoToUrl("https://surfstage.service-now.com");
+            driver.Quit();
+        }
+    }
+}
