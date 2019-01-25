1b79ab1f9dbdb1c2373edaf97a9abb903aceaa01
Rathijit Sarkar <rathijit.sarkar@gmail.com>
2019-01-25 02:10:22 -0800

    added easyrepro code

--- SeleniumPoC3/SeleniumPoC3/SampleTest.cs
+++ SeleniumPoC3/SeleniumPoC3/SampleTest.cs
@@ -3,6 +3,10 @@ using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+
+
 
 namespace SeleniumPoC3
 {
@@ -12,13 +16,20 @@ namespace SeleniumPoC3
         [TestMethod]
         public void TestMethod1()
         {
-            TextWriterTraceListener myListener = new TextWriterTraceListener("TextWriterOutput.log", "myListener");
+            //TextWriterTraceListener myListener = new TextWriterTraceListener("TextWriterOutput.log", "myListener");
 
-            IWebDriver driver = new ChromeDriver(@"C:\chromedriver");
-            driver.Navigate().GoToUrl("https://www.servicenow.com/");
-            myListener.WriteLine(driver.PageSource); 
-            myListener.Flush();
-            driver.Quit();
+           // IWebDriver driver = new ChromeDriver(@"C:\chromedriver");
+          //  driver.Navigate().GoToUrl("https://www.servicenow.com/");
+          //  myListener.WriteLine(driver.PageSource); 
+         //   myListener.Flush();
+          //  driver.Quit();
+
+            using(var xmBrowser = new Browser(new BrowserOptions { BrowserType= BrowserType.Chrome
+            }))
+            {
+                xmBrowser.LoginPage.Login(new Uri("https://www.servicenow.com/"),
+                    "admin".ToSecureString(), "admin".ToSecureString());
+            }
         }
     }
 }
